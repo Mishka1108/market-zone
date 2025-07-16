ee11043ea4b8a41da217a667b8899e90f5da9625
Mishka1108 <mr.mishka3003@gmail.com>
2025-07-16 11:43:26 -0700

    envi

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:10000/api', // Update with your actual API URL
+    apiUrl: 'https://fullproject-xtbq.onrender.com/api', // Update with your actual API URL
   };
